f8743fcc3a3307d58f6f533c17d4cb5ff876c69c
Adam Morsi <adam.morsi@yahoo.com>
2019-12-01 15:35:45 +0200

    add product item component

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,12 +2,20 @@ import React from 'react';
 import {FlatList, Text} from 'react-native';
 import {connect} from "react-redux";
 import {getAvailableProducts} from "../../store/selectors/productsSelectors";
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreen = ({availableProducts}) => {
     return (
         <FlatList data={availableProducts}
                   keyExtractor={(item) => item.id}
-                  renderItem={itemData => <Text>{itemData.item.title}</Text>}/>
+                  renderItem={itemData => <ProductItem image={itemData.item.imageUrl}
+                                                       title={itemData.item.title}
+                                                       price={itemData.item.price}
+                                                       onAddToCart={() => {
+                                                       }}
+                                                       onViewDetail={() => {
+                                                       }}
+                  />}/>
     );
 };
 

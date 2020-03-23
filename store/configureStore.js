53eadf133f9e8b840ef3ab868ab1461ddb923864
Adam Morsi <adam.morsi@yahoo.com>
2020-03-23 15:54:07 +0200

    add redux-thunk package and enable it

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,7 +1,12 @@
-import {createStore} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 
-const configureStore = () => createStore(rootReducer, composeWithDevTools());
+const configureStore = () =>
+  createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+  );
 
 export default configureStore;

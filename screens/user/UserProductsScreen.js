c6aa8947e094f2099c170135534cbb053bf232e1
Adam Morsi <adam.morsi@yahoo.com>
2020-03-24 13:01:03 +0200

    handle errors on delete product

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { FlatList, Button, Alert } from 'react-native';
 //selectors
-import { getUserProducts } from '../../store/selectors/productsSelectors';
+import {
+  getUserProducts,
+  getAdminErrorMessage
+} from '../../store/selectors/productsSelectors';
 //actions
-import { deleteProduct } from '../../store/actions/productsActions';
+import {
+  deleteProduct,
+  resetAdminErrorMessage
+} from '../../store/actions/productsActions';
 //constants
 import Colors from '../../constants/Colors';
 //components
 import ProductItem from '../../components/shop/ProductItem';
+import LoadingIcon from '../../components/UI/LoadingIcon';
 
 const UserProductsScreen = ({
   userProducts,
+  adminErrorMessage,
   dispatch,
   navigation: { navigate }
 }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  //show alert if error
+  useEffect(() => {
+    if (adminErrorMessage) {
+      Alert.alert('An error has occured', adminErrorMessage, [
+        { text: 'Okay' }
+      ]);
+    }
+    return () => {
+      dispatch(resetAdminErrorMessage());
+    };
+  }, [adminErrorMessage]);
+
   //navigate to edit screen to edit a product
   const editProductHandler = (id) => {
     navigate('EditProduct', { productId: id });
   };
+
+  const deleteProductHandler = (id) => {
+    Alert.alert('Are you sure?', 'Do you really want to delete this item?', [
+      { text: 'No', style: 'default' },
+      {
+        text: 'Yes',
+        style: 'destructive',
+        onPress: async () => {
+          dispatch(resetAdminErrorMessage());
+          setIsLoading(true);
+          try {
+            await dispatch(deleteProduct(id));
+            setIsLoading(false);
+          } catch (err) {
+            setIsLoading(false);
+          }
+        }
+      }
+    ]);
+  };
+
+  //show loading icon if loading
+  if (isLoading) {
+    return <LoadingIcon />;
+  }
 
   return (
     <FlatList
@@ -28,21 +75,6 @@ const UserProductsScreen = ({
         const {
           item: { id, imageUrl, title, price }
         } = itemData;
-
-        const deleteProductHandler = (id) => {
-          Alert.alert(
-            'Are you sure?',
-            'Do you really want to delete this item?',
-            [
-              { text: 'No', style: 'default' },
-              {
-                text: 'Yes',
-                style: 'destructive',
-                onPress: () => dispatch(deleteProduct(id))
-              }
-            ]
-          );
-        };
 
         return (
           <ProductItem
@@ -69,7 +101,8 @@ const UserProductsScreen = ({
 };
 
 const mapStateToProps = (state) => ({
-  userProducts: getUserProducts({ state })
+  userProducts: getUserProducts({ state }),
+  adminErrorMessage: getAdminErrorMessage({ state })
 });
 
 export default connect(mapStateToProps)(UserProductsScreen);

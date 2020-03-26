81aa37fb2a2d7400fb2fabf842c4d88092204522
Adam Morsi <adam.morsi@yahoo.com>
2020-03-26 09:54:18 +0200

    extract products navigator

---
+++ navigation/ProductsNavigator.js
@@ -0,0 +1,44 @@
+//stack navigator
+import { createStackNavigator } from 'react-navigation-stack';
+//helpers
+import { defaultNavOptions, headerButtonIcon } from './Helpers';
+//components
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import CartScreen from '../screens/shop/CartScreen';
+
+export const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: {
+      screen: ProductsOverviewScreen,
+      navigationOptions: ({ navigation: { navigate, toggleDrawer } }) => ({
+        title: 'All Products',
+        headerLeft: headerButtonIcon({
+          onPressHandler: toggleDrawer,
+          icon: 'menu',
+          buttonTitle: 'Menu'
+        }),
+        headerRight: headerButtonIcon({
+          onPressHandler: () => navigate('Cart'),
+          icon: 'cart',
+          buttonTitle: 'Cart'
+        })
+      })
+    },
+    ProductDetail: {
+      screen: ProductDetailScreen,
+      navigationOptions: ({ navigation: { getParam } }) => ({
+        title: getParam('productTitle')
+      })
+    },
+    Cart: {
+      screen: CartScreen,
+      navigationOptions: {
+        title: 'Your Cart'
+      }
+    }
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -8,51 +8,14 @@ import { createDrawerNavigator } from 'react-navigation-drawer';
 import Colors from '../constants/Colors';
 //helpers
 import { defaultNavOptions, headerButtonIcon, setDrawerIcon } from './Helpers';
+//navigators
+import { ProductsNavigator } from './ProductsNavigator';
 //components
-import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
-import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
-import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
 import EditProductScreen from '../screens/user/EditProductScreen';
 
 //==============navigators==============
-//products stack navigator
-const ProductsNavigator = createStackNavigator(
-  {
-    ProductsOverview: {
-      screen: ProductsOverviewScreen,
-      navigationOptions: ({ navigation: { navigate, toggleDrawer } }) => ({
-        title: 'All Products',
-        headerLeft: headerButtonIcon({
-          onPressHandler: toggleDrawer,
-          icon: 'menu',
-          buttonTitle: 'Menu'
-        }),
-        headerRight: headerButtonIcon({
-          onPressHandler: () => navigate('Cart'),
-          icon: 'cart',
-          buttonTitle: 'Cart'
-        })
-      })
-    },
-    ProductDetail: {
-      screen: ProductDetailScreen,
-      navigationOptions: ({ navigation: { getParam } }) => ({
-        title: getParam('productTitle')
-      })
-    },
-    Cart: {
-      screen: CartScreen,
-      navigationOptions: {
-        title: 'Your Cart'
-      }
-    }
-  },
-  {
-    defaultNavigationOptions: defaultNavOptions
-  }
-);
 
 //orders stack navigator
 const ordersNavigator = createStackNavigator(

2ccba1caf8ea73bedba5db6e43ac096375eec618
Adam Morsi <adam.morsi@yahoo.com>
2020-03-26 10:03:30 +0200

    extract admin navigator

---
+++ navigation/AdminNavigator.js
@@ -0,0 +1,42 @@
+//stack navigator
+import { createStackNavigator } from 'react-navigation-stack';
+//helpers
+import { defaultNavOptions, headerButtonIcon } from './Helpers';
+//components
+import UserProductsScreen from '../screens/user/UserProductsScreen';
+import EditProductScreen from '../screens/user/EditProductScreen';
+
+export const AdminNavigator = createStackNavigator(
+  {
+    UserProducts: {
+      screen: UserProductsScreen,
+      navigationOptions: ({ navigation: { toggleDrawer, navigate } }) => ({
+        title: 'Your Products',
+        headerLeft: headerButtonIcon({
+          onPressHandler: toggleDrawer,
+          icon: 'menu',
+          buttonTitle: 'Menu'
+        }),
+        headerRight: headerButtonIcon({
+          onPressHandler: () => navigate('EditProduct'),
+          icon: 'add',
+          buttonTitle: 'Add'
+        })
+      })
+    },
+    EditProduct: {
+      screen: EditProductScreen,
+      navigationOptions: ({ navigation: { getParam } }) => ({
+        title: getParam('productId') ? 'Edit Product' : 'Add Product',
+        headerRight: headerButtonIcon({
+          onPressHandler: getParam('submit'),
+          icon: 'checkmark',
+          buttonTitle: 'Save'
+        })
+      })
+    }
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,56 +1,15 @@
 //react navigation
 import { createAppContainer } from 'react-navigation';
-//stack navigator
-import { createStackNavigator } from 'react-navigation-stack';
 //drawer navigator
 import { createDrawerNavigator } from 'react-navigation-drawer';
 //constants
 import Colors from '../constants/Colors';
 //helpers
-import { defaultNavOptions, headerButtonIcon, setDrawerIcon } from './Helpers';
+import { setDrawerIcon } from './Helpers';
 //navigators
 import { ProductsNavigator } from './ProductsNavigator';
 import { ordersNavigator } from './OrdersNavigator';
-//components
-import UserProductsScreen from '../screens/user/UserProductsScreen';
-import EditProductScreen from '../screens/user/EditProductScreen';
-
-//==============navigators==============
-//admin stack navigator
-const AdminNavigator = createStackNavigator(
-  {
-    UserProducts: {
-      screen: UserProductsScreen,
-      navigationOptions: ({ navigation: { toggleDrawer, navigate } }) => ({
-        title: 'Your Products',
-        headerLeft: headerButtonIcon({
-          onPressHandler: toggleDrawer,
-          icon: 'menu',
-          buttonTitle: 'Menu'
-        }),
-        headerRight: headerButtonIcon({
-          onPressHandler: () => navigate('EditProduct'),
-          icon: 'add',
-          buttonTitle: 'Add'
-        })
-      })
-    },
-    EditProduct: {
-      screen: EditProductScreen,
-      navigationOptions: ({ navigation: { getParam } }) => ({
-        title: getParam('productId') ? 'Edit Product' : 'Add Product',
-        headerRight: headerButtonIcon({
-          onPressHandler: getParam('submit'),
-          icon: 'checkmark',
-          buttonTitle: 'Save'
-        })
-      })
-    }
-  },
-  {
-    defaultNavigationOptions: defaultNavOptions
-  }
-);
+import { AdminNavigator } from './AdminNavigator';
 
 //drawer navigator
 const shopNavigator = createDrawerNavigator(

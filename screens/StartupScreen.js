02897e80d36ecd355f2624c7c2817e8b285fbc1d
Adam Morsi <adam.morsi@yahoo.com>
2020-03-27 12:25:44 +0200

    add startup screen

---
+++ screens/StartupScreen.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { AsyncStorage } from 'react-native';
+//actions
+import { authenticate } from '../store/actions/authActions';
+//components
+import LoadingIcon from '../components/UI/LoadingIcon';
+
+const StartupScreen = ({ navigation: { navigate }, dispatch }) => {
+  useEffect(() => {
+    const tryLogin = async () => {
+      try {
+        const userData = await AsyncStorage.getItem('userData');
+        if (!userData) {
+          navigate('Auth');
+          return;
+        }
+        const transformedData = JSON.parse(userData),
+          { token, userId, expiryDate } = transformedData,
+          expirationDate = new Date(expiryDate);
+
+        if (expirationDate <= new Date() || !token || !userId) {
+          navigate('Auth');
+          return;
+        }
+        navigate('Shop');
+        dispatch(authenticate({ token, userId }));
+      } catch (err) {}
+    };
+    tryLogin();
+  }, []);
+
+  return <LoadingIcon />;
+};
+
+export default connect()(StartupScreen);
